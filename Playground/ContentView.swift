cf4f93bb4ec7a91b4eb8e3c42cc97e2969c60c5b
CAR SOFTWARE SYSTEMS <CARSOFTWARESYSTEMS@GMAIL.COM>
2024-08-17 06:52:33 +0530

    Initial Commit

---
+++ Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Playground
+//
+//  Created by Ananth on 17/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Ananth on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
